ff3d7381fc8585b10c5682ff219930145ffd883c
Rajesh Kumar <rajesh.rxca@gmail.com>
2020-06-01 06:01:00 +0530

    corrected RunTaskEveryMinute.php and added account field to the user model

--- app/Console/Commands/RunTaskEveryMinute.php
+++ app/Console/Commands/RunTaskEveryMinute.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\User;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
@@ -41,14 +42,14 @@ class RunTaskEveryMinute extends Command
     {
         $allEntries = DB::table('book_user')->get();
 
-        foreach($allEntries as $entry){
+        foreach ($allEntries as $entry) {
             $current_charge = $entry->current_charge;
             $past_charges = $entry->past_charges;
 
             $sum = $current_charge + $past_charges;
 
             $entry->past_charge = $sum;
-            
+
             DB::table('book_user')->where([
                 ['user_id', '=', $entry->user_id],
                 ['book_id', '=', $entry->book_id],
@@ -58,7 +59,39 @@ class RunTaskEveryMinute extends Command
             ]);
         }
 
-        // $allEntries = DB::table('book_user')->get();
 
+        $users = DB::table('book_user')->select('user_id')->distinct()->get()->toArray();
+        // dump($users);
+
+        // foreach($users as $key => $value){
+        //     $users[$key] = $value->toArray();
+        // }
+
+        foreach ($users as $key => $value) {
+
+            $arrayed_value = json_decode(json_encode($value));
+            $id = $arrayed_value->user_id;
+
+            $books = DB::table('book_user')->where([
+                ['user_id', '=', $id],
+            ])->get()->toArray();
+
+            $books = json_decode(json_encode($books));
+            // dd($books);
+
+            $sum = 0;
+
+            foreach ($books as $book) {
+                $book = json_decode(json_encode($book), true);
+
+                // dd($book);
+                $charge = $book['past_charges'];
+                $sum = $sum + $charge;
+            }
+
+            User::find($id)->update([
+                'account' => $sum,
+            ]);
+        }
     }
 }
